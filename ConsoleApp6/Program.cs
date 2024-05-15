9d0c90fcbfdf0ebd4aae3f133474c9acd6179e97
William Oviedo <oviedo7898@gmail.com>
2024-05-15 08:23:08 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,80 @@
+﻿interface IAnimal
+{
+    void AnimalSound();
+    void AnimalType();
+    void sleep();
+}
+
+class Pig : IAnimal
+{
+    public void AnimalSound()
+    {
+        // The body of animalSound() is provided here
+        Console.WriteLine("The pig says: wee wee");
+    }
+
+    public void AnimalType()
+    {
+        Console.WriteLine("The pig is a mammal");
+    }
+
+    public void sleep()
+    {
+        Console.WriteLine("Zzz");
+    }
+}
+class Dog : IAnimal
+{
+    public void AnimalSound()
+    {
+        // The body of animalSound() is provided here
+        Console.WriteLine("The dog says: wau");
+    }
+
+    public void AnimalType()
+    {
+        Console.WriteLine("The dog is a mammal");
+    }
+
+    public void sleep()
+    {
+        Console.WriteLine("Zzz");
+    }
+}
+class Cat : IAnimal
+{
+    public void AnimalSound()
+    {
+        // The body of animalSound() is provided here
+        Console.WriteLine("The cat says: miau");
+    }
+
+    public void AnimalType()
+    {
+        Console.WriteLine("The cat is a mammal");
+    }
+
+    public void sleep()
+    {
+        Console.WriteLine("Zzz");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pig myPig = new Pig();  // Create a Pig object
+        myPig.AnimalSound();
+        myPig.AnimalType();
+        myPig.sleep();
+
+        Dog myDog = new Dog();
+        myDog.AnimalSound();
+        myDog.AnimalType();
+        myDog.sleep();
+
+        Cat myCat = new Cat();
+        myCat.AnimalSound();
+        myCat.AnimalType();
+    }
+}
